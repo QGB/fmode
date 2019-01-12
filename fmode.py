4b3e1da6278732befeede3dbbdabf9d268d61cf6
QGB <qgbcs1@gmail.com>
2019-01-12 17:06:45 +0800

    fix len r

--- fmode.py
+++ fmode.py
@@ -7,6 +7,6 @@ for i in fs:
 		r.append([i,os.stat(i)])
 	except Exception as e:
 		print(i,e)
-print('scaned {} files'.format(r))
+print('scaned {} files'.format(len(r) )  )
 with open('fm.pickle','wb') as f:
 	pickle.dump(obj=r,file=f)
